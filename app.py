c3ec9f24815b52a60f0a04293b2f48ed3c2754fc
Robert Freeman <6842762+robertefreeman@users.noreply.github.com>
2020-05-18 09:29:17 -0400

    Update page text to test code scanning
    Update page text to test code scanning

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ redis = Redis(host='redis', port=6379)
 def hello():
     count = redis.incr('hits')
     hostname = socket.gethostname()
-    verbiage = "Hello IRS!! I have been seen " + str(count) + " times, served by Countainer: " + hostname
+    verbiage = "Hello World!! I have been seen " + str(count) + " times, served by Countainer: " + hostname
     # return 'HELLO Robert! I have been seen {} times.\n'.format(count)
     return verbiage
 
